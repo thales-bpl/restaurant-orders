f0601f0eec0465e1aa1bf9578fe57e31cf87a756
thales-bpl <thales.bplima@gmail.com>
2022-11-03 07:03:57 -0300

    feat: get_least_busy_day(), get_busiest_day(), get_dish_count_by_customer()

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,41 +1,5 @@
-import csv
 from reader import reader
 from track_orders import TrackOrders
-
-
-def most_ordered_by_customer(customer, data):
-    pass
-
-
-def dish_count_by_customer(customer, data):
-    pass
-
-
-# def never_ordered_by_customer(customer, data):
-#     menu = set(
-#         ['pizza', 'hamburguer', 'coxinha', 'misto-quente']
-#         )
-#     dishes_tried = set()
-
-#     for name, dish, _ in data:
-#         if name == customer:
-#             dishes_tried.add(dish)
-
-#     return menu.difference(dishes_tried)
-
-
-# def days_absent_by_customer(customer, data):
-#     working_days = set(
-#         ['segunda-feira', 'terça-feira', 'quarta-feira',
-#         'quinta-feira', 'sexta-feira', 'sabado']
-#         )
-#     days_visited = set()
-
-#     for name, _, day in data:
-#         if name == customer:
-#             days_visited.add(day)
-
-#     return working_days.difference(days_visited)
 
 
 def analyze_log(path_to_file):
@@ -45,7 +9,9 @@ def analyze_log(path_to_file):
     for name, order, day in customer_data:
         data_list.add_new_order(name, order, day)
 
-    most_ordered = data_list.get_most_ordered_dish_per_customer('maria')
-    burger_count = data_list.get_dish_count_by_customer('arnaldo', 'hamburguer')
-    never_ordered = data_list.get_never_ordered_per_customer('joao')
-    days_absent = data_list.get_days_never_visited_per_customer('joao')
+    # most_ordered = data_list.get_most_ordered_dish_per_customer('maria')
+    # burger_count = data_list.get_dish_count_by_customer(
+    #     'arnaldo', 'hamburguer'
+    #     )
+    # never_ordered = data_list.get_never_ordered_per_customer('joao')
+    # days_absent = data_list.get_days_never_visited_per_customer('joao')
--- src/track_orders.py
+++ src/track_orders.py
@@ -9,7 +9,20 @@ class TrackOrders:
         self._data.append(customer, order, day)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        order_count_dict = {}
+        most_frequent = self._data[0][1]
+
+        for name, dish, _ in self._data:
+            if name == customer:
+                if dish not in order_count_dict:
+                    order_count_dict[dish] = 1
+                else:
+                    order_count_dict[dish] += 1
+            
+                if order_count_dict[dish] > order_count_dict[most_frequent]:
+                    most_frequent = dish
+
+        return most_frequent
 
     def get_never_ordered_per_customer(self, customer):
         menu = set(
@@ -37,10 +50,40 @@ class TrackOrders:
         return working_days_set.difference(days_visited_set)
 
     def get_busiest_day(self):
-        pass
+        day_count_dict = {}
+        busiest_day = self._data[0][2]
+
+        for _, _, day in self._data:
+            if day not in day_count_dict:
+                day_count_dict[day] = 1
+            else:
+                day_count_dict[day] += 1
+        
+            if day_count_dict[day] > day_count_dict[busiest_day]:
+                busiest_day = day
+
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        day_count_dict = {}
+        least_busy_day = self._data[0][2]
 
-    def get_dish_count_by_customer(self):
-        pass
+        for _, _, day in self._data:
+            if day not in day_count_dict:
+                day_count_dict[day] = 1
+            else:
+                day_count_dict[day] += 1
+        
+            if day_count_dict[day] < day_count_dict[least_busy_day]:
+                least_busy_day = day
+
+        return least_busy_day
+
+    def get_dish_count_by_customer(self, customer, order):
+        dish_count = 0
+
+        for name, dish, _ in self._data:
+            if name == customer and dish == order:
+                dish_count += 1
+        
+        return dish_count
